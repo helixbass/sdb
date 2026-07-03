f2125a4874bbbe4e0d5c4953a1e9905d77aea645
Julian Rosse <julian@helixbass.net>
2026-07-03 08:55:42 -0400

    working on setup

--- src/process.cpp
+++ src/process.cpp
@@ -1,22 +1,22 @@
 #include <libsdb/process.hpp>
+#include <libsdb/error.hpp>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-std::unique_ptr<sdb::process> sdb::process::launch(
-	std::filesystem::path path) {
+std::unique_ptr<sdb::process> sdb::process::launch(std::filesystem::path path) {
 	pid_t pid;
 	if ((pid = fork()) < 0) {
-		// Error: fork failed
+		error::send_errno("fork failed");
 	}
 
 	if (pid == 0) {
 		if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
-			// Error: tracing failed
+			error::send_errno("Tracing failed");
 		}
 		if (execlp(path.c_str(), path.c_str(), nullptr) < 0) {
-			// Error: exec failed
+			error::send_errno("exec failed");
 		}
 	}
 
@@ -29,10 +29,10 @@ std::unique_ptr<sdb::process> sdb::process::launch(
 std::unique_ptr<sdb::process> sdb::process::attach(
 	pid_t pid) {
 	if (pid == 0) {
-		// Error: Invalid PID
+		error::send("Invalid PID");
 	}
 	if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
-		// Error: Could not attach
+		error::send_errno("Could not attach");
 	}
 
 	std::unique_ptr<process> proc (new process(pid, /*terminate_on_end_=*/false));
