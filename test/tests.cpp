add7a415acd19a079e6a13d797493b0ac2ca1fbc
Julian Rosse <julian@helixbass.net>
2026-07-04 10:39:43 -0400

    working on setup

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,49 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libsdb/process.hpp>
 
-TEST_CASE("validate environment") {
-	REQUIRE(true);
+using namespace sdb;
+
+namespace {
+	bool process_exists(pid_t pid);
+}
+
+#include <fstream>
+
+namespace {
+	char get_process_status(pid_t pid) {
+		std::ifstream stat("/proc/" + std::to_string(pid) + "/stat");
+		std::string data;
+		std::getline(stat, data);
+		auto index_of_last_parenthesis = data.rfind(')');
+		auto index_of_status_indicator = index_of_last_parenthesis + 2;
+		return data[index_of_status_indicator];
+	}
+}
+
+TEST_CASE("process::launch() success", "[process]") {
+	auto proc = process::launch("yes");
+	REQUIRE(process_exists(proc->pid()));
+}
+
+#include <sys/types.h>
+#include <signal.h>
+
+namespace {
+	bool process_exists(pid_t pid) {
+		auto ret = kill(pid, 0);
+		return ret != -1 and errno != ESRCH;
+	}
+}
+
+#include <libsdb/error.hpp>
+
+TEST_CASE("process::launch() no such program", "[process]") {
+	REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
+}
+
+
+TEST_CASE("process::attach() success", "[process]") {
+	auto pid = /* launch program without attaching */;
+	auto proc = process::attach(pid);
+	REQUIRE(get_process_status(pid) == 't');
 }
