3577fa03bf0926aa0f191ec2b8f7eebafb3f9e9f
Julian Rosse <julian@helixbass.net>
2026-07-05 08:08:31 -0400

    working on setup

--- test/tests.cpp
+++ test/tests.cpp
@@ -41,7 +41,6 @@ TEST_CASE("process::launch() no such program", "[process]") {
 	REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
 }
 
-
 TEST_CASE("process::attach() success", "[process]") {
 	// auto target = process::launch("targets/run_endlessly", false);
 	auto target = process::launch("build/test/targets/run_endlessly", false);
@@ -51,4 +50,30 @@ TEST_CASE("process::attach() success", "[process]") {
 
 TEST_CASE("process::attach() invalid PID", "[process]") {
 	REQUIRE_THROWS_AS(process::attach(0), error);
+}
+
+TEST_CASE("process::resume() success", "[process]") {
+	{
+		auto proc = process::launch("build/test/targets/run_endlessly");
+		proc->resume();
+		auto status = get_process_status(proc->pid());
+		auto success = status == 'R' or status == 'S';
+		REQUIRE(success);
+	}
+
+	{
+		auto target = process::launch("build/test/targets/run_endlessly", false);
+		auto proc = process::attach(target->pid());
+		proc->resume();
+		auto status = get_process_status(proc->pid());
+		auto success = status == 'R' or status == 'S';
+		REQUIRE(success);
+	}
+}
+
+TEST_CASE("process::resume() already terminated", "[process]") {
+	auto proc = process::launch("build/test/targets/end_immediately");
+	proc->resume();
+	proc->wait_on_signal();
+	REQUIRE_THROWS_AS(proc->resume(), error);
 }
